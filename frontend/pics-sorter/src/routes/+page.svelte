496da72646a8c8a95f51b5e861fdc06bcdf3b171
Kirill Pinchuk <cybergrind@gmail.com>
2022-12-10 21:58:37 +0300

    better mobile gestures

--- frontend/pics-sorter/src/routes/+page.svelte
+++ frontend/pics-sorter/src/routes/+page.svelte
@@ -2,13 +2,59 @@
   import { onMount } from 'svelte';
   import { picsStore, getPics } from '../stores.ts'
   import { shortcut } from '../hotkeys.ts'
+  import { swipe, pinch } from 'svelte-gestures';
+  import Zoom from 'svelte-zoom'
 
   onMount(async () => {
     await getPics()
   })
 
-  let pics, single
+  let pics, single, index
+  let zoom;
   picsStore.subscribe(value => pics = value)
+
+  const swipeHandler = async (event) => {
+    const { direction } = event.detail;
+    console.log('Direction:', direction)
+
+    if (direction === 'bottom') {
+      location.reload()
+    } else if (direction === 'top') {
+      single = undefined
+      return
+    } else if (direction !== 'left') {
+      return true;
+    }
+    const zz = document.querySelector('#zoomed-img')
+    if (zz.style.transform !== 'matrix(1, 0, 0, 1, 0, 0)'){
+      return
+    }
+    if (index === undefined) {
+       index = pics.images.indexOf(single)
+    }
+    if (index >= 2) {
+       await getPics();
+       index = -1
+    }
+    index += 1
+    single = pics.images[index]
+  }
+
+  const pinchHandler = (event) => {
+     zoom = Math.min(zoom * event.detail.scale, 1)
+  }
+
+  const toggleOrZoomOut = () => {
+    const zz = document.querySelector('#zoomed-img')
+    console.log(zz.style.transform)
+    if (zz && zz.style.transform.indexOf('scale(1)') === -1) {
+      for (let i = 0; i < 10; i++) {
+        zoom.zoomOut()
+      }
+    } else {
+      single = undefined
+    }
+  }
   
 </script>
 
@@ -18,9 +64,12 @@
         use:shortcut={{code: 'KeyF', callback: () => single = undefined }}
         use:shortcut={{code: 'KeyD', callback: () => single = undefined }}
         use:shortcut={{code: 'KeyS', callback: () => single = undefined }}
-        class="full-div"
+        use:swipe={{ timeframe: 300, minSwipeDistance: 80 }}
+        on:swipe={swipeHandler}
+        on:click={toggleOrZoomOut}
      >
-       <img src={single} class="img-full">
+        <Zoom src={single} bind:this={zoom} on:load={() => console.log('on Load')}
+            id="zoomed-img"/>
    </div>
 {:else if pics.images && pics.images.length > 0}
     <div class="container"
@@ -30,7 +79,12 @@
         use:shortcut={{code: 'KeyS', callback: () => single = pics.images[0] }}
     >
     {#each pics.images as image}
-       <img src={image} class="img-fit"/>
+        <img src={image} class="img-fit"
+             alt="some picture"
+             on:click={() => {
+           console.log('Image:', image)
+           single=image
+        }} />
     {/each}
   </div>
 {:else}
@@ -53,6 +107,8 @@
         margin-left: auto;
         margin-right: auto;
         display: block;
+    }
+    .img-zoomed {
     }
     .img-fit {
         max-width: 33%;
