9582d86f8ef20fdc61d31b21491f0a5d509be274
Kirill Pinchuk <cybergrind@gmail.com>
2023-02-22 02:45:49 +0300

    swipe left. untabify

--- frontend/pics-sorter/src/routes/+page.svelte
+++ frontend/pics-sorter/src/routes/+page.svelte
@@ -1,34 +1,34 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
-	import { picsStore, getPics, connectWS, setWinner as setWinnerStore, sendMsg, sameOrientation } from '../stores'
-	import { shortcut } from '../hotkeys'
-	import { swipe, pinch } from 'svelte-gestures'
-	import Zoom from 'svelte-zoom'
+  import { onMount } from 'svelte'
+  import { picsStore, getPics, connectWS, setWinner as setWinnerStore, sendMsg, sameOrientation } from '../stores'
+  import { shortcut } from '../hotkeys'
+  import { swipe, pinch } from 'svelte-gestures'
+  import Zoom from 'svelte-zoom'
 
-	import type { Image } from '../types'
+  import type { Image } from '../types'
 
-	onMount(async () => {
-		await getPics()
-		connectWS()
-	})
+  onMount(async () => {
+    await getPics()
+    connectWS()
+  })
 
-	let w, h
-	let pics: Image[]
-	let single: Image | undefined
-	let index: number | undefined
-	let zoom: Zoom | undefined
+  let w, h
+  let pics: Image[]
+  let single: Image | undefined
+  let index: number | undefined
+  let zoom: Zoom | undefined
 
-	const setSingle = (pic: Image) => {
-		index = pics.indexOf(pic)
-		single = pics[index]
-	}
+  const setSingle = (pic: Image) => {
+    index = pics.indexOf(pic)
+    single = pics[index]
+  }
 
-	picsStore.subscribe((value: Image[]) => {
-		pics = value
-		if (single) {
-			single = pics[index]
-		}
-	})
+  picsStore.subscribe((value: Image[]) => {
+    pics = value
+    if (single) {
+      single = pics[index]
+    }
+  })
 
   const setWinner = (pic: Image) => {
     index = 0
@@ -36,82 +36,78 @@
     console.log('SetWinner: ', pic, index)
   }
 
-	const nextSingle = () => {
-		if (single === undefined) {
-			single = pics[0]
-		}
-		if (index === undefined) {
-			index = pics.indexOf(single)
-		}
-		index = (index + 1) % pics.length
-		single = pics[index]
-	}
+  const nextSingle = () => {
+    if (single === undefined) {
+      single = pics[0]
+    }
+    if (index === undefined) {
+      index = pics.indexOf(single)
+    }
+    index = (index + 1) % pics.length
+    single = pics[index]
+  }
 
-	const prevSingle = () => {
-		if (single === undefined) {
-			single = pics[pics.length - 1]
-		}
-		if (index === undefined) {
-			index = pics.indexOf(single)
-		}
-		index = (index - 1 + pics.length) % pics.length
-		single = pics[index]
-	}
+  const prevSingle = () => {
+    if (single === undefined) {
+      single = pics[pics.length - 1]
+    }
+    if (index === undefined) {
+      index = pics.indexOf(single)
+    }
+    index = (index - 1 + pics.length) % pics.length
+    single = pics[index]
+  }
 
-	const closeSingle = () => {
-		single = undefined
-		index = undefined
-	}
+  const closeSingle = () => {
+    single = undefined
+    index = undefined
+  }
 
-	const swipeHandler = async (event) => {
-		const { direction } = event.detail
-		console.log('Direction:', direction)
+  const swipeHandler = async (event) => {
+    const { direction } = event.detail
+    console.log('Direction:', direction)
 
-		if (direction === 'bottom') {
-			location.reload()
-		} else if (direction === 'top') {
-			single = undefined
-			return
-		} else if (direction !== 'left') {
-			return true
-		}
-		const zz = document.querySelector('#zoomed-img')
-		if (zz?.style.transform !== 'matrix(1, 0, 0, 1, 0, 0)') {
-			return
-		}
-		if (index === undefined) {
-			index = pics.indexOf(single)
-		}
-		if (index >= 2) {
-			await getPics()
-			index = -1
-		}
-		index += 1
-		single = pics[index]
-	}
+    if (direction === 'bottom') {
+      location.reload()
+    } else if (direction === 'top') {
+      single = undefined
+      return
+    }
 
-	const resetZoom = () => {
-		const zz = document.querySelector('#zoomed-img')
-		if (zz && zz.style.transform && zz.style.transform.indexOf('scale(1)') === -1) {
-			for (let i = 0; i < 10; i++) {
-				zoom?.zoomOut()
-			}
-			return true
-		}
-		return false
-	}
+    const zz = document.querySelector('#zoomed-img')
+    if (zz?.style.transform !== 'matrix(1, 0, 0, 1, 0, 0)') {
+      return
+    }
+    if (index === undefined) {
+      index = pics.indexOf(single)
+    }
+    let inc = direction === 'left' ? 1 : 2;
+    index = (index + inc ) % 3
+    single = pics[index]
+  }
 
-	const toggleOrZoomOut = () => {
-		const zz = document.querySelector('#zoomed-img')
-		if (!resetRoom) {
-			closeSingle()
-		}
-	}
+  const resetZoom = () => {
+    const zz = document.querySelector('#zoomed-img')
+    if (zz && zz.style.transform && zz.style.transform.indexOf('scale(1)') === -1) {
+      for (let i = 0; i < 10; i++) {
+        zoom?.zoomOut()
+      }
+      return true
+    }
+    return false
+  }
 
-	const toggleOrientation = async () => {
-		await sendMsg({ event: 'toggle_orientation' })
-		await getPics()
-	}
+  const toggleOrZoomOut = () => {
+    const zz = document.querySelector('#zoomed-img')
+    if (!resetRoom) {
+      closeSingle()
+    }
+  }
+
+  const toggleOrientation = async () => {
+    await sendMsg({ event: 'toggle_orientation' })
+    await getPics()
+  }
 
   const addExtraCount = async () => {
     const extra_count = single.extra_count > 0 ? 1 : 2
@@ -119,156 +115,156 @@
     single.extra_count += extra_count
   }
 
-	$: orientation = w > h ? 'landscape' : 'portrait'
+  $: orientation = w > h ? 'landscape' : 'portrait'
 </script>
 
 <div class="app">
-	{#if single}
-		<div
-			class="container"
-			use:shortcut={{ code: 'Space', callback: async () => closeSingle() }}
-			use:shortcut={{ code: 'KeyF', callback: () => closeSingle() }}
-			use:shortcut={{ code: 'KeyD', callback: () => closeSingle() }}
-			use:shortcut={{ code: 'KeyS', callback: () => closeSingle() }}
+  {#if single}
+    <div
+      class="container"
+      use:shortcut={{ code: 'Space', callback: async () => closeSingle() }}
+      use:shortcut={{ code: 'KeyF', callback: () => closeSingle() }}
+      use:shortcut={{ code: 'KeyD', callback: () => closeSingle() }}
+      use:shortcut={{ code: 'KeyS', callback: () => closeSingle() }}
       use:shortcut={{ code: 'KeyB', callback: () => addExtraCount() }}
-			use:shortcut={{
-				code: 'KeyG',
-				callback: () => {
-					if (single === undefined) {
-						return
-					}
-					setWinner(single)
-				}
-			}}
-			use:shortcut={{
-				code: 'KeyA',
-				callback: () => {
-					nextSingle()
-				}
-			}}
-			use:shortcut={{
-				code: 'KeyZ',
-				callback: () => {
-					prevSingle()
-				}
-			}}
-			use:shortcut={{
-				code: 'Digit6',
-				callback: async () => {
-					sendMsg({ event: 'hide', image: single?.path })
-				}
-			}}
       use:shortcut={{
-				code: 'Digit5',
-				callback: async () => {
-					sendMsg({ event: 'restore_last' })
-				}
-			}}
-			on:click={toggleOrZoomOut}
-			aria-hidden="true"
-			use:swipe={{ timeframe: 300, minSwipeDistance: 80, touchAction: 'none' }}
-			on:swipe={swipeHandler}
-		>
-			<Zoom
-				src={single.link}
-				bind:this={zoom}
-				on:load={() => console.log('on Load')}
-				id="zoomed-img"
-			/>
-		</div>
-	{:else if pics && pics.length > 0}
-		<div
-			class="container"
-			use:shortcut={{ code: 'Space', callback: async () => getPics() }}
-			use:shortcut={{ code: 'KeyR', callback: () => setWinner(pics[2]) }}
-			use:shortcut={{ code: 'KeyE', callback: () => setWinner(pics[1]) }}
-			use:shortcut={{ code: 'KeyW', callback: () => setWinner(pics[0]) }}
-			use:shortcut={{ code: 'KeyF', callback: () => setSingle(pics[2]) }}
-			use:shortcut={{ code: 'KeyD', callback: () => setSingle(pics[1]) }}
-			use:shortcut={{ code: 'KeyS', callback: () => setSingle(pics[0]) }}
-			use:shortcut={{ code: 'KeyA', callback: () => setSingle(pics[0]) }}
+        code: 'KeyG',
+        callback: () => {
+          if (single === undefined) {
+            return
+          }
+          setWinner(single)
+        }
+      }}
       use:shortcut={{
-				code: 'Digit5',
-				callback: async () => {
-					sendMsg({ event: 'restore_last' })
-				}
-			}}
-		>
-			{#each pics as image}
-				<div class="img-fit">
-					<!--><button on:click={() => setWinner(image)}>win</button><-->
+        code: 'KeyA',
+        callback: () => {
+          nextSingle()
+        }
+      }}
+      use:shortcut={{
+        code: 'KeyZ',
+        callback: () => {
+          prevSingle()
+        }
+      }}
+      use:shortcut={{
+        code: 'Digit6',
+        callback: async () => {
+          sendMsg({ event: 'hide', image: single?.path })
+        }
+      }}
+      use:shortcut={{
+        code: 'Digit5',
+        callback: async () => {
+          sendMsg({ event: 'restore_last' })
+        }
+      }}
+      on:click={toggleOrZoomOut}
+      aria-hidden="true"
+      use:swipe={{ timeframe: 300, minSwipeDistance: 80, touchAction: 'none' }}
+      on:swipe={swipeHandler}
+    >
+      <Zoom
+        src={single.link}
+        bind:this={zoom}
+        on:load={() => console.log('on Load')}
+        id="zoomed-img"
+      />
+    </div>
+  {:else if pics && pics.length > 0}
+    <div
+      class="container"
+      use:shortcut={{ code: 'Space', callback: async () => getPics() }}
+      use:shortcut={{ code: 'KeyR', callback: () => setWinner(pics[2]) }}
+      use:shortcut={{ code: 'KeyE', callback: () => setWinner(pics[1]) }}
+      use:shortcut={{ code: 'KeyW', callback: () => setWinner(pics[0]) }}
+      use:shortcut={{ code: 'KeyF', callback: () => setSingle(pics[2]) }}
+      use:shortcut={{ code: 'KeyD', callback: () => setSingle(pics[1]) }}
+      use:shortcut={{ code: 'KeyS', callback: () => setSingle(pics[0]) }}
+      use:shortcut={{ code: 'KeyA', callback: () => setSingle(pics[0]) }}
+      use:shortcut={{
+        code: 'Digit5',
+        callback: async () => {
+          sendMsg({ event: 'restore_last' })
+        }
+      }}
+    >
+      {#each pics as image}
+        <div class="img-fit">
+          <!--><button on:click={() => setWinner(image)}>win</button><-->
 
-					<img
-						src={image.link}
-						alt="some picture"
-						aria-hidden="true"
-						on:click={() => {
-							console.log('Image:', image)
-							single = image
-						}}
-					/>
-				</div>
-			{/each}
-		</div>
-	{:else}
-		<p>loading...</p>
-	{/if}
-	<div class="bottom-nav">
-		{#if single}
-			<button on:click|preventDefault={() => nextSingle()}>next</button>
-			<button on:click={() => prevSingle()}>prev</button>
+          <img
+            src={image.link}
+            alt="some picture"
+            aria-hidden="true"
+            on:click={() => {
+              console.log('Image:', image)
+              single = image
+            }}
+          />
+        </div>
+      {/each}
+    </div>
+  {:else}
+    <p>loading...</p>
+  {/if}
+  <div class="bottom-nav">
+    {#if single}
+      <button on:click|preventDefault={() => nextSingle()}>next</button>
+      <button on:click={() => prevSingle()}>prev</button>
 
-			<button on:click={() => setWinner(single)}>winner</button>
+      <button on:click={() => setWinner(single)}>winner</button>
       <button on:click={() => addExtraCount(single)}>++</button>
-			<button on:click={() => closeSingle()}>X</button>
+      <button on:click={() => closeSingle()}>X</button>
       <span>{single.elo_rating}/{single.extra_count}</span>
-		{:else}
-			<button on:click={toggleOrientation}>
-				{#if $sameOrientation}
-					Orientation {$sameOrientation}
-				{:else}
-					No Orientation
-				{/if}
-			</button>
-			<span>{w}x{h} => {orientation}</span>
+    {:else}
+      <button on:click={toggleOrientation}>
+        {#if $sameOrientation}
+          Orientation {$sameOrientation}
+        {:else}
+          No Orientation
+        {/if}
+      </button>
+      <span>{w}x{h} => {orientation}</span>
       <span> {#each pics as image} |{image.elo_rating}/{image.extra_count} {/each}</span>
       <button on:click={() => sendMsg({event: 'build_top10'})}>Build Top10</button>
       <button on:click={() => sendMsg({event: 'touch_restart'})}>Reindex</button>
-		{/if}
-	</div>
+    {/if}
+  </div>
 </div>
 <svelte:window bind:innerWidth={w} bind:innerHeight={h} />
 
 <style>
-	.app {
-		display: grid;
-		grid-template-rows: 1fr auto;
-		min-height: 100%;
-	}
+  .app {
+    display: grid;
+    grid-template-rows: 1fr auto;
+    min-height: 100%;
+  }
 
-	.bottom-nav {
-		z-index: 2;
-		rid-row-start: 2;
-		grid-row-end: 3;
-	}
-	.bottom-nav > button {
-		height: 45px;
-		margin-right: 20px;
-	}
+  .bottom-nav {
+    z-index: 2;
+    rid-row-start: 2;
+    grid-row-end: 3;
+  }
+  .bottom-nav > button {
+    height: 45px;
+    margin-right: 20px;
+  }
 
-	.container {
-		flex: 1 0 auto;
-		display: flex;
-		flex-flow: row wrap;
-		padding: 0;
-	}
+  .container {
+    flex: 1 0 auto;
+    display: flex;
+    flex-flow: row wrap;
+    padding: 0;
+  }
 
-	.img-fit {
-		max-width: 33%;
-		max-height: 32%;
-	}
-	.img-fit > img {
-		max-width: 33vw;
-		max-height: 97vh;
-	}
+  .img-fit {
+    max-width: 33%;
+    max-height: 32%;
+  }
+  .img-fit > img {
+    max-width: 33vw;
+    max-height: 97vh;
+  }
 </style>
--- frontend/pics-sorter/src/stores.ts
+++ frontend/pics-sorter/src/stores.ts
@@ -8,14 +8,14 @@ export const sameOrientation = writable(0)
 
 let _pics: Image[]
 picsStore.subscribe((value) => {
-	_pics = value
+  _pics = value
 })
 
 export async function getPics() {
-	const response = await axios.get(`${window.location.origin}/api/pics/`)
-	console.log(response.data)
-	picsStore.set(response.data.images)
-	sameOrientation.set(response.data.same_orientation)
+  const response = await axios.get(`${window.location.origin}/api/pics/`)
+  console.log(response.data)
+  picsStore.set(response.data.images)
+  sameOrientation.set(response.data.same_orientation)
 }
 
 const MAX_EVENTS = 10
@@ -25,35 +25,35 @@ export const events = writable([])
 
 const GET_PICS_EVENTS = ['rate_success', 'hide_success', 'restore_success']
 export const addEvent = (event) => {
-	events.update((events) => {
-		events.unshift(event)
-		return events.slice(0, MAX_EVENTS)
-	})
-	console.log('Event: ', event)
+  events.update((events) => {
+    events.unshift(event)
+    return events.slice(0, MAX_EVENTS)
+  })
+  console.log('Event: ', event)
 
-	if (GET_PICS_EVENTS.includes(event.event)) {
-		getPics()
-	}
+  if (GET_PICS_EVENTS.includes(event.event)) {
+    getPics()
+  }
 }
 
 let _ws
 
 export const connectWS = () => {
-	if (_ws !== undefined) return
+  if (_ws !== undefined) return
 
-	events.subscribe((evts) => (window.evts = evts))
-	_ws = new ReconnectingWebSocket(`ws://${window.location.host}/ws`)
-	_ws.addEventListener('message', (event) => {
-		addEvent(JSON.parse(event.data))
-	})
+  events.subscribe((evts) => (window.evts = evts))
+  _ws = new ReconnectingWebSocket(`ws://${window.location.host}/ws`)
+  _ws.addEventListener('message', (event) => {
+    addEvent(JSON.parse(event.data))
+  })
 }
 
 export const setWinner = (winner:Image) => {
-	const loosersObjs = _pics.filter((pic) => pic.path !== winner.path)
-	const loosers = loosersObjs.map((v) => v.path)
-	_ws.send(JSON.stringify({ event: 'rate', winner: winner.path, loosers }))
+  const loosersObjs = _pics.filter((pic) => pic.path !== winner.path)
+  const loosers = loosersObjs.map((v) => v.path)
+  _ws.send(JSON.stringify({ event: 'rate', winner: winner.path, loosers }))
 }
 
 export const sendMsg = (msg:{event: string}) => {
-	_ws.send(JSON.stringify(msg))
+  _ws.send(JSON.stringify(msg))
 }
